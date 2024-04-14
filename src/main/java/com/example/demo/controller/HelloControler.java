9b4b22b5e54073155887a4ee0a1802fd85eba780
KPGDevs <KPGDevs@gmail.com>
2024-04-14 16:01:21 +0200

    bug: changed endpoint /appi to /api

--- src/main/java/com/example/demo/controller/HelloControler.java
+++ src/main/java/com/example/demo/controller/HelloControler.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/appi")
+@RestController("/api")
 public class HelloControler {
     @GetMapping("/hello")
     public String sayHello() {
