c0131512d7cb22f832b51d5252f6e07769571085
KPGDevs <KPGDevs@gmail.com>
2024-04-14 17:03:27 +0200

    changed to hellllo

--- src/main/java/com/example/demo/controller/HelloControler.java
+++ src/main/java/com/example/demo/controller/HelloControler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api")
 public class HelloControler {
-    @GetMapping("/hello")
+    @GetMapping("/hellllo")
     public String sayHello() {
         return "Hi!, How Are You?";
     }
